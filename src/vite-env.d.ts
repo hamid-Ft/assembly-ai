e681ff2d5fe2cadbc545ad42a1db9e00285912fb
HamidFt <hamidfattahi.a@gmail.com>
2023-11-13 13:44:58 +0330

    checkup type error issue

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
-  // more env variables...
-  readonly VITE_APP_API: string;
+  [key: string]: string;
+  VITE_API_KEY?: string;
 }
 
 interface ImportMeta {
